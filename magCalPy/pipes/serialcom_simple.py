eef6153c00500f67c5bb21aaa8649f89b4dc9304
MrOneTwo <ciesielskimm@gmail.com>
2017-05-23 14:16:02 +0200

    Changed the way data is being read form serial... performance is bad.

--- magCalPy/pipes/serialcom_simple.py
+++ magCalPy/pipes/serialcom_simple.py
@@ -4,18 +4,40 @@
 import serial
 import time
 import os
+import re
+import collections
+
+DOING = 'CALIBRATION OF MAGNETOMETER'
+CONSUMER_PRESENT = True
 
 FRAME_SIZE_BIN = 4
 FRAME_SIZE_2 = 32
 PREAMBLE_BIN = 0xAA
-PREAMBLE_2 = 'M'
-PREAMBLE_3 = 'A'
 
 baud_rate = 115200
 fifo_file = None
 fifo_file_path = '/tmp/serialPort.fifo'
 data = 'a\n'
 running = True
+
+incoming_data_buffer = collections.deque(maxlen=30)
+
+if DOING == 'CALIBRATION OF MAGNETOMETER':
+    PREAMBLE = '#'
+    pattern = re.compile("#M-R="
+                         "[+-]?[0-9]+[\.][0-9][0-9]"
+                         ","
+                         "[+-]?[0-9]+[\.][0-9][0-9]"
+                         ","
+                         "[+-]?[0-9]+[\.][0-9][0-9]")
+else:
+    PREAMBLE = '#'
+    pattern = re.compile("#YPR="
+                         "[+-]?[0-9]+[\.][0-9][0-9]"
+                         ","
+                         "[+-]?[0-9]+[\.][0-9][0-9]"
+                         ","
+                         "[+-]?[0-9]+[\.][0-9][0-9]")
 
 try:
     fifo_file = os.mkfifo(fifo_file_path)
@@ -24,7 +46,7 @@ except OSError as e:
         print(' -ex--->  Fifo file exists.')
         pass
 
-serial_port = serial.Serial('/dev/sensor_uart',
+serial_port = serial.Serial('/dev/ttyUSB1',
                             baud_rate,
                             timeout=0.5)
 
@@ -52,60 +74,65 @@ def main():
     :returns: TODO
 
     """
+    global data
     global running
     global working_properly_trust
-
-    # n = 0
-    # while n < 1000:
-    # serial_port.write('mag\n')
-    # n += 1
-    preamble_character = ''
 
     while running:
         # clear data
         data_bytes = ''
-        data = list()
 
-        if serial_port.in_waiting > 0 and preamble_character != PREAMBLE_2 and preamble_character != PREAMBLE_3:
-            preamble_character = serial_port.read(1)
-        else:
-            pass
-
-        if (preamble_character == PREAMBLE_2 or preamble_character == PREAMBLE_3) and serial_port.in_waiting >= 31:
-            preamble_character = ''
-            data_bytes = serial_port.read(31)
-
-        # get rid of 'AG.'
-        data_bytes = data_bytes[3:]
-        # read the data
-        data_bytes = data_bytes.strip()
+        if serial_port.in_waiting > 0:
+            incoming_data_buffer.append(serial_port.read(1))
 
         try:
-            data = data_bytes.split('.')
-            data = map(int, data)
-        except:
+            if incoming_data_buffer[0] == PREAMBLE:
+                if pattern.match(''.join(incoming_data_buffer)):
+                    try:
+                        split_end_index = list(incoming_data_buffer).index('#', 1)
+                    except ValueError:
+                        continue
+                    data_bytes = ''.join(incoming_data_buffer)
+                    data_bytes = data_bytes[5:split_end_index]
+                    data_bytes = data_bytes.strip()
+                    data_bytes = data_bytes.rstrip()
+                    data_bytes = data_bytes.replace('\r', '')
+                    data_bytes = data_bytes.replace('\n', '')
+                    data = data_bytes.split(',')
+                    if DOING == 'CALIBRATION OF MAGNETOMETER':
+                        data = map(float, data)
+                        data = map(int, data)
+                    else:
+                        data = map(float, data)
+                    print('Y: {0}, P: {1}, R: {2}'.format(data[0], data[1], data[2]))
+
+                if CONSUMER_PRESENT and ((type(data[0]) == int) or (type(data[0]) == float)):
+                    try:
+                        fifo_file = open(fifo_file_path, 'w')
+                        fifo_file.write(str(data[0]))
+                        fifo_file.write(';')
+                        fifo_file.write(str(data[1]))
+                        fifo_file.write(';')
+                        fifo_file.write(str(data[2]))
+                        fifo_file.write('\n')
+                        fifo_file.close()
+                    except IOError as e:
+                        fifo_file.close()
+                        pass
+
+                else:
+                    # print('Pattern not matching.')
+                    continue
+        except IndexError:
             continue
 
-        print('Data = {0}'.format(data))
-        try:
-            fifo_file = open(fifo_file_path, 'w')
-            fifo_file.write(str(data[0]))
-            fifo_file.write(';')
-            fifo_file.write(str(data[1]))
-            fifo_file.write(';')
-            fifo_file.write(str(data[2]))
-            fifo_file.write('\n')
-            fifo_file.close()
-        except IOError as e:
-            fifo_file.close()
-            pass
 
 
 if __name__ == '__main__':
-    try:
-        if start() is True:
-            main()
-    except Exception as e:
-        print(' -ex--->  Exception, being: {0}'.format(e))
-        serial_port.close()
+    #  try:
+    if start() is True:
+        main()
+    #  except Exception as e:
+        #  print(' -ex--->  Exception, being: {0}'.format(e))
+        #  serial_port.close()
 
